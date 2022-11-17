33cf96bfc6ffe618c233fc6f284599456327f18f
Facundo Wade Jacobs <wadefacundo@gmail.com>
2022-11-16 21:11:25 -0300

    fixed desafio-01

--- desafio-01/desafio.js
+++ desafio-01/desafio.js
@@ -1,22 +1,71 @@
 class ProductManager {
 
-    static nextProductId = 1;
+    static nextProductId = 0;
 
     constructor() {
         this.products = [];
     }
 
-    addProduct(product) {
-        if(this.products.findIndex(p => p.code === product.code) !== -1) {
-            console.log(`El objeto con el código: ${product.code} ya existe.`)
-            return;
+    getNextId = () => {
+        ProductManager.nextProductId++;
+        return ProductManager.nextProductId;
+    }
+
+    isValidProduct(product) {
+        if(product.title?.trim() === '') {
+            console.log('ERROR. Detalle: El título no puede estar vacío.');
+            return false;
         }
 
-        product.id = ProductManager.nextProductId;
-        
-        this.products.push(product);
 
-        ProductManager.nextProductId++;
+        if(product.description?.trim() === '') {
+            console.log('ERROR. Detalle: La descripción no puede estar vacía.');
+            return false;
+        }
+
+        if(!product.price) {
+            console.log('ERROR. Detalle: El precio no puede estar vacío.');
+            return false;
+        }
+
+        if(product.thumbnail?.trim() === '') {
+            console.log('ERROR. Detalle: La ruta de imagen no puede estar vacía.');
+            return false;
+        }
+
+        if(product.code?.trim() === '') {
+            console.log('ERROR. Detalle: El código no puede estar vacío');
+            return false;
+        }
+
+        if(!product.stock) {
+            console.log('ERROR. Detalle: El stock no puede estar vacío.');
+            return false;
+        }
+
+        if(this.products.findIndex(p => p.code === product.code) !== -1) {
+            console.log(`ERROR. Detalle: El objeto con el código: ${product.code} ya existe.`)
+            return false;
+        }
+
+        return true;
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        if(this.isValidProduct(product)) {
+            product.id = this.getNextId();
+
+            this.products.push(product);
+        }
     }
 
     getProducts() {
@@ -42,27 +91,13 @@ console.log('Obteniendo productos...');
 console.log(pm.getProducts());
 
 console.log('Creando nuevo producto...');
-pm.addProduct({
-    title: 'producto prueba',
-    description: 'Este es un producto prueba',
-    price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
-    stock: 25
-})
+pm.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
 
 console.log('Obteniendo productos...');
 console.log(pm.getProducts());
 
 console.log('Agregando el mismo producto una vez más...');
-pm.addProduct({
-    title: 'producto prueba',
-    description: 'Este es un producto prueba',
-    price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
-    stock: 25
-})
+pm.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
 
 console.log('Obtenemos el producto con id 1...');
 console.log(pm.getProductById(1));
@@ -71,17 +106,13 @@ console.log('Obtenemos el producto con id 3728...');
 pm.getProductById(3728);
 
 console.log('Agregando un producto más...');
-pm.addProduct({
-    title: 'producto prueba 2',
-    description: 'Este es un producto prueba 2',
-    price: 250,
-    thumbnail: 'Sin imagen',
-    code: 'def456',
-    stock: 40
-})
+pm.addProduct('producto prueba 2', 'Este es un producto prueba 2', 250, 'Sin imagen', 'def456', 40)
 
 console.log('Obteniendo productos...');
 console.log(pm.getProducts());
 
 console.log('Obtenemos el producto con id 2...');
 console.log(pm.getProductById(2));
+
+console.log('Agregando un producto más, con el precio vacío...');
+pm.addProduct('producto prueba 3', 'Este es un producto prueba 3', null, 'Sin imagen', 'def456', 40)
